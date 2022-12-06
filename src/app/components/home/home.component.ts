53bb6b52b5a79fc61fa2616b3f8260664568f741
Sevda <58671217+svdsmsk@users.noreply.github.com>
2022-12-06 16:15:45 +0300

    create istatics

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,9 @@
+import { IApplicationGetAllModel } from './../../models/response/application/application-getall-model';
+import { IApplicantGetAllModel } from 'src/app/models/response/applicant/applicant-getall-model';
+import { ApplicationService } from 'src/app/services/application.service';
+import { ApplicantService } from 'src/app/services/applicant.service';
+import { IInstructorGetAllModel } from './../../models/response/instructor/instructor-getall-model';
+import { InstructorService } from 'src/app/services/instructor.service';
 import { BootcampService } from './../../services/bootcamp.service';
 import { IBootcampGetAllModel } from './../../models/response/bootcamp/bootcamp-getall-model';
 import { Component, OnInit } from '@angular/core';
@@ -5,20 +11,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  bootcamps:IBootcampGetAllModel[]=[];
-  constructor(private bootcampService:BootcampService) { }
+  bootcamps: IBootcampGetAllModel[] = [];
+  instructors: IInstructorGetAllModel[] = [];
+  applicants: IApplicantGetAllModel[] = [];
+  applications: IApplicationGetAllModel[] = [];
+  constructor(
+    private bootcampService: BootcampService,
+    private instructorService: InstructorService,
+    private applicantService: ApplicantService,
+    private applicationService: ApplicationService
+  ) {}
 
   ngOnInit(): void {
-    this.getBootcamps()
+    this.getBootcamps();
+    this.getInstructors();
+    this.getApplicants();
+    this.getApplications();
   }
 
-  getBootcamps(){
-    this.bootcampService.getBootcamps().subscribe((data)=>{
-      this.bootcamps = data
-    })
+  getBootcamps() {
+    this.bootcampService.getBootcamps().subscribe((data) => {
+      this.bootcamps = data;
+    });
+  }
+  getInstructors() {
+    this.instructorService.getInstructors().subscribe((data) => {
+      this.instructors = data;
+    });
+  }
+  getApplicants() {
+    this.applicantService.getApplicants().subscribe((data) => {
+      this.applicants = data;
+    });
+  }
+  getApplications() {
+    this.applicationService.getApplications().subscribe((data) => {
+      this.applications = data;
+    });
   }
 }
